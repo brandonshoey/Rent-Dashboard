5b096d015383c9efac23da2ba8bf14c13b9af317
Brandon Shuey <67021427+brandonshoey@users.noreply.github.com>
2023-06-02 10:27:44 -0500

    Add files via upload

--- src/app.py
+++ src/app.py
@@ -1,10 +1,12 @@
-from dash import Dash, dcc, html, Input, Output
+from dash import Dash, dash_table, dcc, html, Input, Output, State
+import plotly.express as px
 from fundamentals import getMarkets, getMarketData
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
+import pandas as pd
 import dash
 
-# Reading in CoStar Market data
+# Reading in CoStar market level data
 markets = getMarkets()
 
 app = Dash(__name__)
@@ -12,26 +14,27 @@ server = app.server
 
 app.layout = html.Div(
     children = [
-        html.H1(children="Rent Dashboard"),
+        html.H1(children="Rent Forecasting Dashboard"),
 
         dcc.Dropdown(id="my-dropdown", multi=False,
-                    options = [{"label": x, "value": x} for x in markets],
-                    value = "All"),
+                     options = [{"label": x, "value": x} for x in markets],
+                     value = "All"),
 
-        dcc.Graph(id="bar-chart-output", figure={})
+        dcc.Graph(id="bar-chart-output", figure={}),
     ]
 )
 
-# Generate the chart
+# Callback and function that generates a chart
 @app.callback(
     Output(component_id="bar-chart-output", component_property="figure"),
     [Input(component_id="my-dropdown", component_property="value")],
     prevent_initial_callback=False
 )
-def update_graph(val_chosen):
+def update_my_graph(val_chosen):
     if len(val_chosen) > 0:
+        print(f"Building chart for {val_chosen}")
         df = getMarketData(val_chosen)
-        fig = make_subplots(specs = [[{"secondary_y": True}]])
+        fig = make_subplots(specs=[[{"secondary_y": True}]])
 
         fig.add_trace(
             go.Scatter(mode="lines+markers", x=df["Date"], y=df["RentGrowth"], name="Rent Growth"),
@@ -39,14 +42,14 @@ def update_graph(val_chosen):
         )
         fig.add_trace(
             go.Scatter(mode="lines+markers", x=df["Date"], y=df["AvailableSF"], name="Available SqFt"),
-            secondary_y=True,
+            secondary_y=True
         )
         fig.update_layout(
-            title_text="<b>Rent and Available SqFt Analysis</b>"
+            title_text="<b>Rent Forecast Dynamics</b>"
         )
         return fig
     elif len(val_chosen) == 0:
         raise dash.exceptions.PreventUpdate
-    
+
 if __name__ == "__main__":
     app.run_server(debug=True)
--- src/fundamentals.py
+++ src/fundamentals.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import datetime
 
-# Localize a timeframe for analysis
+# Localizing a timeframe for analysis
 start = np.datetime64("2009-12-31")
 end = np.datetime64(datetime.datetime.today().date())
 
@@ -12,13 +12,11 @@ df["Date"] = pd.to_datetime(df["Date"], format="%Y-%m-%d")
 df = df.loc[(df["Date"] > start)]
 df = df.loc[(df["Date"] < end)]
 
-# Set up the markets
 def getMarkets():
     markets = df["MarketName"].unique().tolist()
     markets.append("All")
     return markets
 
-# Extract data from markets
 def getMarketData(market):
     if market == "All":
         data = df.copy()
@@ -30,3 +28,5 @@ def getMarketData(market):
         data = df.copy()
         data = data.loc[(df["MarketName"] == market)]
         return data[["Date", "AvailableSF", "RentGrowth"]]
+    
+print(getMarketData("All"))
